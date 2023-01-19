f7dfd75765c1f87e9e38b32bd4bdac22afe6261b
Caleb Grant <geocoug@gmail.com>
2023-01-19 07:07:15 -0800

    + ruff config, lint with ruff
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- app/main.py
+++ app/main.py
@@ -23,35 +23,36 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 
-data_dict = {
+DATA_DICT = {
     "col1": [1, 2, 3, 4, 5],
     "col2": ["a", "b", "c", "d", "e"],
     "col3": [True, True, False, True, True],
 }
 
 NEW_COLUMN = "new_col"
-new_data = ["z", "y", "x", "w", "v"]
+NEW_DATA = ["z", "y", "x", "w", "v"]
 
 
 class Data:
     """Data class."""
 
-    def __init__(self, data: dict) -> None:
+    def __init__(self: "Data", data: dict) -> None:
+        """Object intialization."""
         self.df = pd.DataFrame(data)
         self.columns = list(self.df.columns)
 
-    def add_dataframe_column(self, col_name: str) -> None:
+    def add_dataframe_column(self: "Data", col_name: str) -> None:
         """Add a new dataframe column."""
         self.df[col_name] = np.nan
 
-    def fill_dataframe_column(self, col_name: str, data: list) -> None:
+    def fill_dataframe_column(self: "Data", col_name: str, data: list) -> None:
         """Fill an empty dataframe column."""
         self.df[col_name] = data
 
 
 if __name__ == "__main__":
-    dtable = Data(data_dict)
+    dtable = Data(DATA_DICT)
     dtable.add_dataframe_column(NEW_COLUMN)
-    dtable.fill_dataframe_column(NEW_COLUMN, new_data)
+    dtable.fill_dataframe_column(NEW_COLUMN, NEW_DATA)
     logger.info(dtable.columns)
     logger.info(dtable.df)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,24 @@
 import pandas as pd
 import pytest
 
-from app.main import NEW_COLUMN, Data, data_dict, new_data
+from app.main import DATA_DICT, NEW_COLUMN, NEW_DATA, Data
 
 
 def test_init_df():
     """Test dataframe initiation."""
-    data = Data(data_dict)
+    data = Data(DATA_DICT)
     assert data.df is not None, "dataframe not initiated"
 
 
 def test_init_columns():
     """Test dataframe dimensions."""
-    data = Data(data_dict)
+    data = Data(DATA_DICT)
     assert len(data.columns) == 3, "incorrect dataframe dimensions"
 
 
 def test_add_dataframe_column():
     """Test that a new dataframe column was added."""
-    data = Data(data_dict)
+    data = Data(DATA_DICT)
     data.add_dataframe_column(NEW_COLUMN)
     assert (
         NEW_COLUMN not in data.columns or len(data.columns) != 4
@@ -27,7 +27,7 @@ def test_add_dataframe_column():
 
 def test_add_dataframe_column_vals():
     """Test that a column was filled with NaN."""
-    data = Data(data_dict)
+    data = Data(DATA_DICT)
     data.add_dataframe_column(NEW_COLUMN)
     for idx, row in enumerate(data.df[NEW_COLUMN]):
         assert pd.isna(
@@ -37,11 +37,11 @@ def test_add_dataframe_column_vals():
 
 def test_fill_dataframe_column():
     """Check that a column was filled with correct values."""
-    data = Data(data_dict)
+    data = Data(DATA_DICT)
     data.add_dataframe_column(NEW_COLUMN)
-    data.fill_dataframe_column(NEW_COLUMN, new_data)
+    data.fill_dataframe_column(NEW_COLUMN, NEW_DATA)
     for idx, row in enumerate(data.df[NEW_COLUMN]):
-        assert row == new_data[idx], "column contains incorrect fill values"
+        assert row == NEW_DATA[idx], "column contains incorrect fill values"
 
 
 if __name__ == "__main__":
