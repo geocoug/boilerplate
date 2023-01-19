b0bc8b3b528dabca94c95800ce3cca250b34dbd9
Caleb Grant <geocoug@gmail.com>
2023-01-19 07:12:53 -0800

    ignore S101 (bandit), return type annotations in test_main.py
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,19 +4,19 @@ import pytest
 from app.main import DATA_DICT, NEW_COLUMN, NEW_DATA, Data
 
 
-def test_init_df():
+def test_init_df() -> None:
     """Test dataframe initiation."""
     data = Data(DATA_DICT)
     assert data.df is not None, "dataframe not initiated"
 
 
-def test_init_columns():
+def test_init_columns() -> None:
     """Test dataframe dimensions."""
     data = Data(DATA_DICT)
     assert len(data.columns) == 3, "incorrect dataframe dimensions"
 
 
-def test_add_dataframe_column():
+def test_add_dataframe_column() -> None:
     """Test that a new dataframe column was added."""
     data = Data(DATA_DICT)
     data.add_dataframe_column(NEW_COLUMN)
@@ -25,7 +25,7 @@ def test_add_dataframe_column():
     ), "dataframe column does not exist"
 
 
-def test_add_dataframe_column_vals():
+def test_add_dataframe_column_vals() -> None:
     """Test that a column was filled with NaN."""
     data = Data(DATA_DICT)
     data.add_dataframe_column(NEW_COLUMN)
@@ -35,7 +35,7 @@ def test_add_dataframe_column_vals():
         ), f"column initiated with value other than NaN {row}"
 
 
-def test_fill_dataframe_column():
+def test_fill_dataframe_column() -> None:
     """Check that a column was filled with correct values."""
     data = Data(DATA_DICT)
     data.add_dataframe_column(NEW_COLUMN)
